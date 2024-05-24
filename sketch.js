4ebb21f7e648aaf31501b1eba10cf080f82648e4
A.P. Cammeraat <cam@emmauscollege.nl>
2024-05-24 21:34:50 +0000

    in loop

--- sketch.js
+++ sketch.js
@@ -12,7 +12,10 @@
 ///<reference path="p5.global-mode.d.ts" />
 "use strict"
 
-const MUTATION_CHANGE = 0.05;
+const MUTATION_CHANGE = 0.5;
+const GENERATION_SIZE = 50;
+const ELITE_SIZE = 4;
+
 
 let label;
 let teRadenWoordInput;
@@ -49,8 +52,8 @@ function setup() {
 
   nextGenerationButton = createButton("Next generation");
   nextGenerationButton.position(450, 120);
-  nextGenerationButton.attribute("disabled", true)
-  nextGenerationButton.mouseClicked(nextIteration);
+  nextGenerationButton.attribute("disabled", true);
+  nextGenerationButton.mouseClicked(iterate);
 }
 
 
@@ -78,33 +81,70 @@ function start() {
   for (let i = 0; i < currentGeneration.length; i++) {
     calculateSolutionFitness(currentGeneration[i])
   }
+}
 
-  console.log("beste oplossing: ", bestCurrentSolution());
+function iterate() {
+  let generationNumber = 1;
+  while (currentGeneration[0].value != teRadenWoord) {
+    generationNumber++;
+    nextIteration();
+    console.log("generatie: ", generationNumber);
+  }
+  alert("gevonden: " + currentGeneration[0].value)
+  nextGenerationButton.attribute("disabled", true);
+  startButton.removeAttribute("disabled");
 }
 
 function nextIteration() {
-  let newGeneration = []
+  let newGeneration = [];
 
   // creëer een nieuwe generatie:
   // kies twee oplossingen
   calculateTotalScore();
-  let solA = rouletteWheelSelection()
-  let solB = rouletteWheelSelection()
-  while (solA === solB) {
-    solB = rouletteWheelSelection()
+
+  for (let i = 0; i < ELITE_SIZE; i++) {
+    newGeneration.push(currentGeneration[i]);
   }
 
-  console.log(solA);
-  console.log(solB);
+  console.log("Twee beste oplossingen meenemen naar nieuwe generatie", newGeneration)
 
-  // maak twee nieuwe oplossingen door uitwisseling
-  // en zet die in de nieuwe generatie
-  
-  
+  for (let i = 0; i < ((GENERATION_SIZE - ELITE_SIZE) / 2); i++) {
+    let solA = rouletteWheelSelection()
+    let solB = rouletteWheelSelection()
+    while (solA === solB) {
+      solB = rouletteWheelSelection()
+    }
+
+    // maak twee nieuwe oplossingen door uitwisseling
+    // en zet die in de nieuwe generatie
+    let offspring = solutionWithCrossOver(solA, solB);
+    for (let i = 0; i < offspring.length; i++) {
+      mutate(offspring[i]);
+    }
+
+    newGeneration = newGeneration.concat(solutionWithCrossOver(solA, solB));
+  }
+
   // vervang de oude generatie
   currentGeneration = newGeneration
+  for (let i = 0; i < currentGeneration.length; i++) {
+    calculateSolutionFitness(currentGeneration[i]);
+  }
+
+  currentGeneration.sort((a, b) => {
+    if (a.score < b.score) {
+      return 1
+    }
+    else if (a.score > b.score) {
+      return -1
+    }
+    else {
+      return 0
+    }
+  })
 
 
+  console.log("gesorteerde generatie: ", currentGeneration);
 }
 
 
@@ -115,14 +155,14 @@ function createRandomSolution() {
   for (var i = 0; i < 8; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
-  return {value: result}
+  return { value: result }
 }
 
 
 function createFirstGeneration() {
   currentGeneration = [];
 
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < GENERATION_SIZE; i++) {
     currentGeneration.push(createRandomSolution());
   }
 
@@ -134,7 +174,7 @@ function calculateSolutionFitness(solution) {
   let score = 1;
   for (let i = 0; i < 8; i++) {
     if (solution.value[i] === teRadenWoord[i]) {
-      score = score * 2;
+      score = score + 1;
       console.log("letter is hetzelfde");
     }
   }
@@ -158,7 +198,7 @@ function rouletteWheelSelection() {
   let rouletteNumber = random(currentTotalScore);
   let searchNumber = 0;
 
-  for(let i = 0; i<currentGeneration.length; i++) {
+  for (let i = 0; i < currentGeneration.length; i++) {
     searchNumber = searchNumber + currentGeneration[i].score
     if (searchNumber >= rouletteNumber) {
       return currentGeneration[i];
@@ -167,21 +207,38 @@ function rouletteWheelSelection() {
 }
 
 
-function bestCurrentSolution() {
+function bestCurrentSolutions() {
   let bestSolution = currentGeneration[0];
+  let secondBestSolution = currentGeneration[0];
 
   for (let i = 0; i < currentGeneration.length; i++) {
     if (currentGeneration[i].score > bestSolution.score) {
+      secondBestSolution = bestSolution;
       bestSolution = currentGeneration[i];
     }
   }
 
-  return bestSolution;
+  return [bestSolution, secondBestSolution];
 }
 
 
-function solutionWidthCrossOver(solA, solB) {
+function solutionWithCrossOver(solA, solB) {
+  let genes = solA.value + solB.value;
 
+  let newSolA = { value: "" };
+  let newSolB = { value: "" };
+
+  for (let i = 0; i < genes.length; i++) {
+    let toA = Math.floor(random(2));
+    if ((toA === 1 && newSolA.value.length < 8) || newSolB.value.length === 8) {
+      newSolA.value = newSolA.value + genes[i];
+    }
+    else {
+      newSolB.value = newSolB.value + genes[i];
+    }
+  }
+
+  return [newSolA, newSolB];
 }
 
 
@@ -189,7 +246,13 @@ function mutate(solution) {
   let roll = random(1)
   if (roll < MUTATION_CHANGE) {
     let characterIndex = floor(random(8))
-    solution.value[characterIndex] = getRandomLowerCaseLetter()
+    let first = solution.value.substring(0, characterIndex - 1);
+    let last = "";
+    if (characterIndex < 6) {
+      last = solution.value.substring(characterIndex)
+    }
+
+    solution.value = first + getRandomLowerCaseLetter() + last
   }
 }
 
